27023e5121b9e7c33c52a9465845327340cb746f
RetliveAdore <lizaterop@gmail.com>
2024-07-10 22:50:12 +0800

    修改编译脚本

--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,5 @@
 if [ ! -d "./CrystalCore/" ];then git submodule add https://github.com/RetliveAdore/CrystalCore;fi
+git submodule update --init
 git submodule update --remote
 cp ./platform/linux/makefile ./makefile
 make build
